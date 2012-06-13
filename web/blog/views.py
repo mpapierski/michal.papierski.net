fa9655ad8238a9416bc48a6d28d61376bb6ff82f
Michał Papierski <michal@papierski.net>
2012-06-13 21:56:21 +0200

    Blog app views, and templates

--- web/blog/views.py
+++ web/blog/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from django.views.generic import ListView
+
+from blog.models import Post
+
+class PostListView(ListView):
+	'''
+	Posts list
+	'''
+	model = Post
+	
--- web/web/urls.py
+++ web/web/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'web.views.home', name='home'),
+    url(r'^$', include('blog.urls')),
     # url(r'^web/', include('web.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
