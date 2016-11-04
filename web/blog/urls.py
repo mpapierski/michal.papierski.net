d148deb239b98a4f0a1fe5ee7bc8b540cf1667ac
Michał Papierski <michal@papierski.net>
2016-11-04 11:54:55 +0100

    Remove old django app.

--- web/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-
-from blog.views import PostListView
-
-urlpatterns = patterns('',
-	#url('^(?P<year>\d+)/(?P<month>\d+)/(?P<day>\d+)/(?P<slug>[_-\w\d]+)/$',
-	url(r'^$', PostListView.as_view()),
-)
